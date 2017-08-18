2187f7a4c0da91fbb328994bf7eaccf80a2222fd
yangwei <yang971460565@126.com>
2017-08-18 22:12:15 +0800

    add curl command

--- ci/application/controllers/test/Test.php
+++ ci/application/controllers/test/Test.php
@@ -16,4 +16,16 @@
 			echo now() . PHP_EOL;
 			echo date('Y-m-d H:i:s');
 		}
+
+		public function curl() {
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, 'www.baidu.com');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_HEADER, 0);
+			$output = curl_exec($ch);
+			//var_dump($output);
+			$info = curl_getinfo($ch);
+			var_dump($info);
+			curl_close($ch);
+		}
 	}
